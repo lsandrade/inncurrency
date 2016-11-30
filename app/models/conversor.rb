0524fb42736dbf864aa65b7ecd376f4d256ed916
Luan Andrade <luansandrade92@hotmail.com>
2016-11-30 14:35:52 -0200

    get exchange base

--- app/models/conversor.rb
+++ app/models/conversor.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+require 'json'
+
+## Important: This documentation has been deprecated and is no longer valid. For detailed reference about API integration, usage guides and language examples please visit: currencylayer.com/documentation
+
 class Conversor < ActiveRecord::Base
   @@API_KEY = "86a2d0814613df7f6459de5656a60970"
 
   def hello_api
     @@API_KEY
+  end
+
+  def get_exchange
+    stream = open(
+      "http://apilayer.net/api/historical?"+
+      "&date=2016-11-29"+
+      "&currencies=EUR,BRL,ARS"+
+      "&access_key="+@@API_KEY
+      )
+    data = stream.read
+    json = JSON.parse(data)
+
   end
 end
--- spec/models/conversor_spec.rb
+++ spec/models/conversor_spec.rb
@@ -3,7 +3,29 @@ require 'rails_helper'
 RSpec.describe Conversor, type: :model do
   context "Hello" do
     it "hello world" do
-      puts "My gosh!"
+      puts "Give my money back!"
+    end
+  end
+
+  context "Get exchange" do
+    it "29-11-2016" do
+      conversor = Conversor.new
+      exchange = conversor.get_exchange
+
+      expect(exchange).to eql(JSON.parse("{
+        \"success\":true,
+        \"terms\":\"https:\/\/currencylayer.com\/terms\",
+        \"privacy\":\"https:\/\/currencylayer.com\/privacy\",
+        \"historical\":true,
+        \"date\":\"2016-11-29\",
+        \"timestamp\":1480463999,
+        \"source\":\"USD\",
+        \"quotes\":{
+          \"USDEUR\":0.938973,
+          \"USDBRL\":3.454798,
+          \"USDARS\":15.540096
+        }
+      }"))
     end
   end
 end
