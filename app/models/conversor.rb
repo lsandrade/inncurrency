75bda5232054227f74efabe9b9f7a62f364456e4
Luan Andrade <luansandrade92@hotmail.com>
2016-11-30 19:25:36 -0200

    final conversor

--- app/models/conversor.rb
+++ app/models/conversor.rb
@@ -1,7 +1,6 @@
 require 'open-uri'
 require 'json'
 
-## Important: This documentation has been deprecated and is no longer valid. For detailed reference about API integration, usage guides and language examples please visit: currencylayer.com/documentation
 
 class Conversor < ActiveRecord::Base
   @@API_KEY = "86a2d0814613df7f6459de5656a60970"
