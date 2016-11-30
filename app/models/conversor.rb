87f0afd9e10091e7b7468c6da9ddcf55f560251a
Luan Andrade <luansandrade92@hotmail.com>
2016-11-30 13:40:23 -0200

    API KEY

--- app/models/conversor.rb
+++ app/models/conversor.rb
@@ -1,2 +1,7 @@
 class Conversor < ActiveRecord::Base
+  @@API_KEY = "86a2d0814613df7f6459de5656a60970"
+
+  def hello_api
+    @@API_KEY
+  end
 end
--- spec/models/conversor_spec.rb
+++ spec/models/conversor_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Conversor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Hello" do
+    it "hello world" do
+      puts "My gosh!"
+    end
+  end
 end
